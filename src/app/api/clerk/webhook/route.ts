eda8494651c892f049561a8546a29d62be169858
Alextechie <ngangaalex795@gmail.com>
2024-12-18 21:35:26 +0300

    Ignore type errors

--- src/app/api/clerk/webhook/route.ts
+++ src/app/api/clerk/webhook/route.ts
@@ -1,36 +1,42 @@
 import { db } from "@/server/db";
 
 export const POST = async (req: Request) => {
-    const {data} = await req.json();
-    const {last_name: lastName, first_name: firstName, image_url :imageUrl, id} =  data;
-    const email = data.email_addresses[0].email_address
+    const { data } = await req.json();
+    const { last_name: lastName, first_name: firstName, image_url: imageUrl, id } = data;
+    const email = data.email_addresses[0].email_address;
     console.log(lastName);
 
-    // check if user exists
-    const checkIfExists = await db.user.findUnique({
-        where: {
-            id
+    try {
+        // check if user exists
+        const checkIfExists = await db.user.findUnique({
+            where: {
+                id
+            }
+        })
+        if (checkIfExists) {
+            return Response.json({
+                message: "User already exists",
+                status: 400
+            })
         }
-    })
-    if(checkIfExists){
+
+        // store the data onto the db
+        const user = await db.user.create({
+            data: {
+                id,
+                email,
+                lastName,
+                firstName,
+                imageUrl
+            }
+        })
+        console.log("user created:", user)
+    } catch (err) {
+        console.log("Error adding user to the database");
         return Response.json({
-            message: "User already exists",
-            status: 400
+            message: "Error adding user to the database"
         })
     }
 
-    // store the data onto the db
-    const user = await db.user.create({
-        data: {
-            id,
-            email,
-            lastName,
-            firstName,
-            imageUrl
-        }
-    })
-
-    console.log("user created:", user)
-
-    return new Response("Webhook received", {status: 200});
+    return new Response("Webhook received", { status: 200 });
 }
